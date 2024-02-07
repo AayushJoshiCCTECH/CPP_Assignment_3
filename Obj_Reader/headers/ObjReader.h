44fc934f329d0ab32b1deca693a95932b02abce7
AayushJoshiCCTECH <157628596+AayushJoshiCCTECH@users.noreply.github.com>
2024-02-07 19:37:08 +0530

    Delete Obj_Reader directory

--- Obj_Reader/headers/ObjReader.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Triangulation.h" // Include the header file for Triangulation
-
-class ObjReader
-{
-public:
-    // Method to read data from an OBJ file and populate a Triangulation object
-    void readObj(Triangulation &triangulation);
-};
