810fce86df8b66138274aaa042aff46f0ddae7e4
AayushJoshiCCTECH <157628596+AayushJoshiCCTECH@users.noreply.github.com>
2024-02-07 19:37:35 +0530

    Delete STLtoOBJ directory

--- STLtoOBJ/headers/STLtoOBj.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../headers/Triangulation.h" // Include the header file for Triangulation
-
-class StlToObj
-{
-public:
-    void writer(Triangulation &triangulation); // Method to convert STL triangulation to OBJ format
-};
